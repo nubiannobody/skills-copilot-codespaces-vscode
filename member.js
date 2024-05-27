005050080c430bdd795a141bd4132e674191ed6d
Kween <kweenyoallhiada@gmail.com>
2024-05-27 21:50:01 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,12 @@
+function skillsMember() {
+    var member = {
+        name: "Abhishek",
+        age: 25,
+        skills: ["JavaScript", "React", "Node", "Angular", "Vue"],
+        details: {
+            address: "Mumbai",
+            pincode: 400001
+        }
+    };
+    return member;
+}
